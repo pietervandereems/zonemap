5a46e05ce592d5553e19e00d28e09d2adbc419b2
Pieter van der Eems <pieter@eemco.nl>
2015-02-08 17:06:08 +0100

    Changed tileserver to local server

--- map.js
+++ map.js
@@ -4,23 +4,14 @@
 require(['leaflet'], function (L) {
     'use strict';
     var mapElm = document.getElementById('map'), // Dom Elements
-        map = L.map(mapElm).setView([12.971599, 77.594563], 11), // Leaflet Variables
-        waterColor,
-        tonerLines;
+        map = L.map(mapElm).setView([12.971599, 77.594563], 12), // Leaflet Variables
+        zone;
 
-    waterColor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        subdomains: 'abcd',
-        minZoom: 1,
+    zone = L.tileLayer('http://zone.mekton.nl/tiles/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        minZoom: 12,
         maxZoom: 18
     });
-    tonerLines = L.tileLayer('http://{s}.tile.stamen.com/toner-lines/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        subdomains: 'abcd',
-        minZoom: 0,
-        maxZoom: 20
-    });
 
-    waterColor.addTo(map);
-    tonerLines.addTo(map);
+    zone.addTo(map);
 });
