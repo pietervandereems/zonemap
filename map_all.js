1a393b987b8a61536ceaf0f4c598f422db3ad9b3
Pieter van der Eems <p.van.der.eems@interactiveblueprints.nl>
2015-02-12 09:52:02 +0100

    allMarked, locations to array, loop locations to add markers

--- map_all.js
+++ map_all.js
@@ -6,8 +6,90 @@ require(['leaflet'], function (L) {
     var mapElm = document.getElementById('map'), // Dom Elements
         map = L.map(mapElm).setView([12.971599, 77.594563], 12), // Leaflet Variables
         zone,
-        locate,
-        updateMarker;
+        locations;
+
+    locations = [
+        {
+            'lat': 12.96967141582902,
+            'lng': 77.59339928627014,
+            'popup': 'Sports'
+        },
+        {
+            'lat': 2.9492,
+            'lng': 77.5860,
+            'popup': 'Botanical Gardens'
+        },
+        {
+            'lat': 12.96219588361988,
+            'lng': 77.57351875305176,
+            'popup': 'Fastolfe Hospital'
+        },
+        {
+            'lat': 13.080230711990428,
+            'lng': 77.57497787475586,
+            'popup': 'Agricultural University'
+        },
+        {
+            'lat': 13.135695543101336,
+            'lng': 77.60300159454346,
+            'popup': 'Airforce'
+        },
+        {
+            'lat': 12.938377236944088,
+            'lng': 77.59669303894043,
+            'popup': 'Mental Sciences'
+        },
+        {
+            'lat': 12.94383544221124,
+            'lng': 77.59581327438354,
+            'popup': 'Dental Sciences'
+        },
+        {
+            'lat': 12.987601322043336,
+            'lng': 77.62922286987305,
+            'popup': 'Army'
+        },
+        {
+            'lat': 12.826363620592424,
+            'lng': 77.5411605834961,
+            'popup': 'Von Braun Park'
+        },
+        {
+            'lat': 12.88510641838721,
+            'lng': 77.52644062042236,
+            'popup': 'Dyson Memorial Park'
+        },
+        {
+            'lat': 12.9977627702671,
+            'lng': 77.63958692550659,
+            'popup': 'Asimov R&D Grounds'
+        },
+        {
+            'lat': 13.071322,
+            'lng': 77.580199,
+            'popup': 'Geology Department'
+        },
+        {
+            'lat': 12.945651,
+            'lng': 77.508995,
+            'popup': 'Geology Department'
+        },
+        {
+            'lat': 12.998599084931548,
+            'lng': 77.59188652038574,
+            'popup': 'Palace'
+        },
+        {
+            'lat': 13.021868409794724,
+            'lng': 77.56609439849854,
+            'popup': 'Institute of Science'
+        },
+        {
+            'lat': 12.951363805094765,
+            'lng': 77.64690399169922,
+            'popup': 'Detector Range Golf Course'
+        }
+    ];
 
     zone = L.tileLayer('https://zone.mekton.nl/tiles/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -15,63 +97,11 @@ require(['leaflet'], function (L) {
         maxZoom: 18
     });
 
-    locate = L.control();
-
-    locate.onAdd = function (map) {
-        var container,
-            list = '<option value="">Location</option>',
-            updateMarker,
-            marker;
-        container = L.DomUtil.create('div', 'locate-container');
-        L.DomEvent.disableClickPropagation(container);
-        this.select = L.DomUtil.create('select', 'select', container);
-        L.DomEvent.disableClickPropagation(this.select);
-        list += '<option value="12.96967141582902,77.59339928627014">Sports</option>';
-        list += '<option value="12.9492,77.5860">Botanical Gardens</option>';
-        list += '<option value="12.96219588361988,77.57351875305176">Fastolfe Hospital</option>';
-        list += '<option value="13.080230711990428,77.57497787475586">Agricultural University</option>';
-        list += '<option value="13.135695543101336,77.60300159454346">Airforce</option>';
-        list += '<option value="12.938377236944088,77.59669303894043">Mental Sciences</option>';
-        list += '<option value="12.94383544221124,77.59581327438354">Dental Sciences</option>';
-        list += '<option value="12.987601322043336,77.62922286987305">Army</option>';
-        list += '<option value="12.826363620592424,77.5411605834961">Von Braun Park</option>';
-        list += '<option value="12.88510641838721,77.52644062042236">Dyson Memorial Park</option>';
-        list += '<option value="12.9977627702671,77.63958692550659">Asimov R&D Grounds</option>';
-        list += '<option value="13.071322,77.580199">Geology Department</option>';
-        list += '<option value="12.945651,77.508995">Geology Department</option>';
-        list += '<option value="12.998599084931548,77.59188652038574">Palace</option>';
-        list += '<option value="13.021868409794724,77.56609439849854">Institute of Science</option>';
-        list += '<option value="12.951363805094765,77.64690399169922">Detector Range Golf Course</option>';
-        this.select.innerHTML = list;
-
-        updateMarker = function (latlng) {
-            if (marker) { // we have a previous marker
-                marker.setLatLng(latlng).update();
-            } else { // Create a new marker
-                marker = new L.Marker([latlng.lat, latlng.lng], {draggable: false});
-                marker.addTo(map);
-            }
-        };
-        this.goToLocation = function (ev) {
-            var coordinates;
-            if (ev.target.options[ev.target.selectedIndex].value.indexOf(',') > -1) {
-                coordinates = ev.target.options[ev.target.selectedIndex].value.split(',');
-                map.setView([coordinates[0], coordinates[1]], 16);
-                updateMarker(L.latLng(coordinates[0], coordinates[1]));
-            }
-        };
-        this.select.addEventListener('change', this.goToLocation);
-        return this.select;
-    };
-    locate.onRemove = function () {
-        this.select.removeEventListener('change', this.goToLocation);
-        console.log('removed');
-    };
-
     zone.addTo(map);
-    locate.addTo(map);
-
-    map.addEventListener('click', function (ev) {
-        console.log('location:', ev);
+    locations.forEach(function (loc) {
+        var marker;
+        marker = new L.Marker([loc.lat, loc.lng], {draggable: false});
+        marker.bindPopup(loc.popup);
+        marker.addTo(map);
     });
 });
